351e19ce4c7dc16311fb268e66f881b0cd6698ef
SOULIKK <42605239+SOULIKK@users.noreply.github.com>
2022-02-23 16:16:32 +0900

    Init project

---
+++ src/main/java/com/giants2/Giants2Application.java
@@ -0,0 +1,13 @@
+package com.giants2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Giants2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Giants2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/giants2/Giants2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giants2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Giants2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
